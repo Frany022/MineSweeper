da383c643ec434dff58202773f45541c2ecce887
Tibi <bajortibor02@gmail.com>
2025-10-05 00:18:18 +0200

    Test for mine placement

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace minesweeper
 {
     public partial class Form1 : Form
@@ -10,6 +12,32 @@ namespace minesweeper
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GameField field = new GameField(9, 9, 10);
+            string boardstr = GetBoardString(field);
+            MessageBox.Show(boardstr, "minefield");
+
+            int mineCount = field.CountMines();
+            MessageBox.Show($"Total mines placed: {mineCount}");
+        }
+
+        private string GetBoardString(GameField field) 
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < field.Rows; i++)
+            {
+                for (int j = 0; j < field.Columns; j++)
+                {
+                    sb.Append(field.cells[i, j].IsMine ? "*" : ".");
+                }
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
         }
     }
 }
